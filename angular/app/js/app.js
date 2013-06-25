f853214b4d15fcf43a69c6614ac4e429692220a1
James Barton <james@revolet.com>
2013-06-25 10:33:06 -0500

    Compute job completion info on the server.  Add bootstrap.

--- angular/app/js/app.js
+++ angular/app/js/app.js
@@ -3,7 +3,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('MapReduce', ['MapReduce.filters', 'MapReduce.services', 'MapReduce.directives', 'MapReduce.controllers']).
     config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/', {templateUrl: 'partials/mapreduce.html', controller: 'MapReduceCtrl'});
+        $routeProvider.when('/', {templateUrl: 'app/partials/mapreduce.html', controller: 'MapReduceCtrl'});
         $routeProvider.otherwise({redirectTo: '/'});
     }])
 ;
--- lib/MapReduceUI.pm
+++ lib/MapReduceUI.pm
@@ -14,7 +14,7 @@ sub startup {
     my $r = $self->routes;
 
     # Normal route to controller
-    $r->get('/' => sub { shift->render_static('index.html') });
+    $r->get('/' => sub { shift->render_static('app/index.html') });
     
     my $json = Mojo::JSON->new;
 
@@ -36,7 +36,7 @@ sub startup {
         my %mapped_counts  = map { $_ => $redis->get("mr-$_-mapped-count")  } keys %job_ids;
         my %reduced_counts = map { $_ => $redis->get("mr-$_-reduced-count") } keys %job_ids;
         my %result_counts  = map { $_ => $redis->get("mr-$_-result-count")  } keys %job_ids;
-        
+       
         my @response = map {{
             id => $_,
             
@@ -45,14 +45,18 @@ sub startup {
             mapped_count  => $mapped_counts{$_},
             reduced_count => $reduced_counts{$_},
             result_count  => $result_counts{$_},
+            
+            input_percent   => sprintf( '%d', 100 * $input_counts{$_}   / $input_totals{$_} ),
+            mapped_percent  => sprintf( '%d', 100 * $mapped_counts{$_}  / $input_totals{$_} ),
+            reduced_percent => sprintf( '%d', 100 * $reduced_counts{$_} / $input_totals{$_} ),
+            result_percent  => sprintf( '%d', 100 * $result_counts{$_}  / $input_totals{$_} ),
+            
+            expires => $redis->ttl("mr-$_-result-count") . ' seconds',
         }} keys %job_ids;
         
         my $bytes = $json->encode(\@response);
         
         $self->send($bytes);
-    #        });
-
-    #    });
     };
 
     
